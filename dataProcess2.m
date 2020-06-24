91e7a7b4e570d00f5262e08d9750e5738d406066
narutomst <narutomst@163.com>
2020-06-24 19:18:19 +0800

    为了节省空间，将导入的原始数据分别保存于handles.UserData.matdata和handles.UserData.gtdata中，而不再输出x3和lbs2，同时从x2和lbs中去除了背景像素。

--- dataProcess2.m
+++ dataProcess2.m
@@ -8,6 +8,20 @@ function [x3,x2,lbs2,lbs,matInfo,gtInfo] = dataProcess2(handles)
     S = whos('-file',matPath);
     matInfo = S;
     x3 = eval((S.name));
+
+    load(gtPath);
+    S = whos('-file',gtPath);
+    gtInfo = S;
+    lbs2 = eval((S.name));
+    lbs2 = double(lbs2);
+    %保存数据
+    handles.UserData.matdata = x3; %未去除背景像素的数据主要是用于可视化显示
+    handles.UserData.gtdata = lbs2;    
+    
+    %将背景像素去除，只留一个0像素
+    a = lbs2~=0;     %1096×715 logical
+    b = find(lbs2);   % 148152 double
+    
     n_dims = ndims(x3);
     % 三维数据转换为二维数据
     if  n_dims == 3
@@ -19,30 +33,24 @@ function [x3,x2,lbs2,lbs,matInfo,gtInfo] = dataProcess2(handles)
         x2 = x3;    %所选择mat数据为二维mat
         x3 = [];
     end
-    
+    x2 = x2(b,:);
     % 获取lbs2，2维的标签数据
-    load(gtPath);
-    S = whos('-file',gtPath);
-    gtInfo = S;
-    lbs2 = eval((S.name));
-    lbs2 = double(lbs2);
+
+    
     % 二维标签转换为一维标签
     n_dims = ndims(lbs2);
     if n_dims == 1                %所选择标签数据为一维标签
         lbs = lbs2;
-%         while (min(lbs(:))==0)  %使lbs最小值为1
-%             lbs = lbs+1;
-%         end
+        lbs = lbs(b,:);
         lbs2 = [];
     elseif n_dims == 2
         lbs = reshape(lbs2,m,1);%将标签按列取出来拼接成一个列向量
                                             %lbs表示一维标签
-%         while (min(lbs(:))==0)  %使lbs最小值为1
-%             lbs = lbs+1;
-%         end                                            
+        lbs = lbs(b,:);               
     elseif n_dims == 3
         lbs = [];
         str = {'所选择标签文件可能有问题，维度为3.';'可查看hmenu4_1.UserData.lbs2'};
         warndlg(str);
     end
+
 end
