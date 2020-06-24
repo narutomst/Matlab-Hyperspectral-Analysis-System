3993f13fb536953da9f274611bb54bbf04af67cf
narutomst <narutomst@163.com>
2020-06-24 15:36:37 +0800

    在给相应打开函数添加了imgGT标志值之后，第1代码块没有漏洞了。 这种情况下将第1代码块开启，第2代码块关闭

--- SeparatePlot2_Callback.m
+++ SeparatePlot2_Callback.m
@@ -1,7 +1,7 @@
 function SeparatePlot2_Callback(hObject, eventdata, handles)
 % 【单独绘制(原始大小)】
 %只有当前图窗口显示有图片，我们才执行。否则啥也不干。
-if ~isempty(findobj(handles,'Type','image')) 
+if 1&&~isempty(findobj(handles,'Type','image')) 
     himage = findobj(handles,'Type','image');
     hmenu3_1 = hObject.Parent.Children(4);
     p = figure();
@@ -16,5 +16,28 @@ if ~isempty(findobj(handles,'Type','image'))
     hscrollpanel = imscrollpanel(p, himage);  
 end    
   
-
+if 0&&~isempty(findobj(handles,'Type','image'))       
+    himage = findobj(handles,'Type','image');
+    
+    p = figure();
+    if ~isempty(findobj(handles,'Tag','imscrollpanel'))
+        if ~isempty(findobj(handles,'Type','colorbar'))
+            %在新的figure中显示GT图片
+            himage = plot1(handles);
+        else
+            %在新的figure中显示普通图片
+            himage = imshow(himage.CData,'Parent',gca);%经过试验，即便加上,'InitialMagnification',200，图片也无法显示到原始大小
+        end
+    else %窗口中有图但是没有滚动条
+        hmenu3_1 = hObject.Parent.Children(4);%也是[适应窗口]
+        if ~hmenu3_1.UserData.imgGT     %查询【适应窗口】操作的对象是普通图还是GT图
+        %  在新的figure中显示普通图片
+            himage = imshow(himage.CData);
+        else
+            %在新的figure中显示GT图片
+            himage = plot1(handles);
+        end
+    end
+    hscrollpanel = imscrollpanel(p, himage);
+end
 end
