fef590910f16ffada567ffc4336f49a7c26631d1
narutomst <narutomst@163.com>
2020-06-24 20:27:25 +0800

    增加了一点，使其即可以绘制已经去除背景像素点的数据，也可以绘制未去除背景像素点的数据

--- ldaPlot.m
+++ ldaPlot.m
@@ -1,42 +1,26 @@
-% clear,close all hidden  %这次要给不同类别的点标注上不同的颜色！
-% clc
-% 输入数据：
-% 降维后的mat数据x2, 其每一列是一种属性（特征）
-% 一维列向量的标签数据lbs, 
-% 再就是cmap
-% ldaPlot和pcaPlot的核心代码都是一样的
-
-% colorBase = [ [1,0,0]; [0,1,0]; [0,0,1]; [1,1,0]; [1,0,1]; [0,1,1]; ...
-%                     [0.5,0,0]; [0,0.5,0];[0,0,0.5]; [0.5,1,0]; [1,0.5,0]; [0.5,0.5,0]; ... 
-%                     [0.5,0,1]; [1,0,0.5]; [0.5,0,0.5]; [0,0.5,1]; [0,1,0.5]; [0,0.5,0.5]; ...
-%                     [0.5,0.5,0.5]; [0.1,0.1,0.1]];
-% str = {'Botswana','Indian_pines_corrected','KSC','Pavia','Salinas_corrected'};
-% str1 = {'Botswana','Indian_pines','KSC','Pavia','Salinas'};
-% 
-% N = length(str);
-% for i = 1 : N
-% S = load([str{i},'_2d_LDA.mat'],'z'); % 
-% input = getfield(S,'z');
-% S1 = load([str1{i},'_gt_Colum.mat'],'lbs'); % 
-% labels = getfield(S1,'lbs');
 function ldaPlot(x2,lbs,handles)
+% x2,lbs是去除背景像素点的数据
+% 绘制其散点图，另一种可是化方式是tSNE
+
+if min(lbs)<=0  %如果数据含有背景像素，则首先去除背景像素
+    b = find(lbs);
+    x2 = x2(b, :);
+    lbs = lbs(b, :);
+end
+
 cmap = handles.UserData.cmap;
 x = x2(:,1);
 y = x2(:,2);
-sz = 5;
-% c = linspace(1,10,length(x));
-% c = repmat([0,0,0],length(x),1);% ones(length(x),1);
-if ~min(lbs)
-    lbs = lbs + 1;
-end
+sz = 5; %散点图上要绘制的点的大小
+
 
 min_c = min(lbs);
 max_c = max(lbs);
-c = zeros(size(lbs,1), 3);
+c = zeros(size(lbs,1), 3); %准备颜色表
 for j = min_c : max_c
     ind = find(lbs==j);
     l_ind = length(ind);
-    c(ind, :) = repmat(cmap(j,:),l_ind,1);
+    c(ind, :) = repmat(cmap(j+1,:),l_ind,1);
 end
 
 figure
