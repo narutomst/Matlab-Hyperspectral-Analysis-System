75ad9ffc72027f676086f28b570ec68f0b7753cc
narutomst <narutomst@163.com>
2020-06-24 15:17:25 +0800

    增加了标志值，hmenu3_1.UserData.imgGT，以指导【查看】功能正确执行

--- openGT.m
+++ openGT.m
@@ -1,7 +1,8 @@
 function [hbox, himage] = openGT(x, handles)%打开*_gt.mat数据
 
-		[hbox, himage] = newPlotGT(x,handles);
-        
-%         handles.UserData.imgGT = double(lbs2);
+	[hbox, himage] = newPlotGT(x,handles);
+    %设置标志值
+    hmenu3_1 = findobj(handles,'Label','适应窗口');
+    hmenu3_1.UserData.imgGT=1;
 			
 end
