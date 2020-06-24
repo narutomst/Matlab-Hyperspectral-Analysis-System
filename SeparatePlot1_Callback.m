76938bc60acee487a3be8d1cecff7a8df07ced15
narutomst <narutomst@163.com>
2020-06-24 15:03:04 +0800

    第1代码块有漏洞，当之前窗口中处理过GT图片时，imgGT就会被置为1，而再次打开mat数据时，图窗中没有colorbar，但是imgGT依然是1，图片会被判定为GT图片。而第2代码块就能正确判定

--- SeparatePlot1_Callback.m
+++ SeparatePlot1_Callback.m
@@ -1,7 +1,21 @@
 function SeparatePlot1_Callback(hObject, eventdata, handles)
 % 【单独绘制(自适应)】
 %只有当前图窗口显示有图片，我们才执行。否则啥也不干。
-if ~isempty(findobj(handles,'Type','image'))       
+if 0&&~isempty(findobj(handles,'Type','image')) 
+    himage = findobj(handles,'Type','image');
+    hmenu3_1 = hObject.Parent.Children(4);
+    p = figure();
+    if isempty(findobj(handles,'Type','colorbar')) && ~hmenu3_1.UserData.imgGT
+    %既不存在colorbar，imgGT又不等于1，则说明当前窗口中为普通图片
+    %在新的figure中以原始大小显示普通图片
+        himage = imshow(himage.CData,'Parent',gca);     
+    else
+    %在新的figure中以原始大小显示GT图片
+        himage = plot1(handles);
+    end
+end
+
+if 1&&~isempty(findobj(handles,'Type','image'))       
     himage = findobj(handles,'Type','image');
     
     figure()
