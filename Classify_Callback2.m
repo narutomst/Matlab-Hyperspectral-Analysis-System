ae5e808848007c605471a398f9c927ef25b15ccf
narutomst <narutomst@163.com>
2020-06-26 14:58:46 +0800

    去掉了lbsTest相关的中间变量img

--- Classify_Callback2.m
+++ Classify_Callback2.m
@@ -173,10 +173,10 @@ end
         %mean()函数按列求平均，所以将行形式转换成列形式
         plot([1, n],[mean(racc(:,1)), mean(racc(:,1))],'--','LineWidth',1.5);
 
-        text(1,mean(racc(:,1))*1.030,['racc1:',num2str(mean(racc(:,1)))]);
+        text(1.05,mean(racc(:,1))*1.030,['racc1:',num2str(mean(racc(:,1)))]);
         try %若racc有两列，即优化前后的数据各占一列，则下面的语句会继续处理第2列数据
             plot([1, n],[mean(racc(:,2)), mean(racc(:,2))],'--','LineWidth',1.5);
-            text(1,mean(racc(:,2))*1.030,['racc2:',num2str(mean(racc(:,2)))]);
+            text(1.05,mean(racc(:,2))*1.030,['racc2:',num2str(mean(racc(:,2)))]);
             legend('best_perf1','best_perf2','best_vperf1','best_vperf2','best_tperf1','best_tperf2','racc1','racc2','Interpreter','none','Location','best');  
             %1表示优化前的数据，2表示优化后的数据
         catch%若racc仅含有一列数据，则按照一列的情形设置图例
@@ -200,10 +200,10 @@ end
         hold on
         acc = 1-racc; %mean()函数按列求平均，所以将行形式转换成列形式
         plot([1, n],[mean(acc(:,1)), mean(acc(:,1))],'--','LineWidth',1.5);
-        text(1,mean(acc(:,1))*1.005,['acc1:',num2str(mean(acc(:,1)))]);
+        text(1.05,mean(acc(:,1))*1.005,['acc1:',num2str(mean(acc(:,1)))]);
         try %若acc有两列，即优化前后的数据各占一列，则下面的语句会继续处理第2列数据
             plot([1, n],[mean(acc(:,2)), mean(acc(:,2))],'--','LineWidth',1.5);
-            text(1,mean(acc(:,2))*1.005,['acc2:',num2str(mean(acc(:,2)))]);
+            text(1.05,mean(acc(:,2))*1.005,['acc2:',num2str(mean(acc(:,2)))]);
             legend('acc_perf1','acc_perf2','acc_vperf1','acc_vperf2','acc_tperf1','acc_tperf2','acc1','acc2','Interpreter','none','Location','best');  
             %1表示优化前的数据，2表示优化后的数据
         catch%若acc仅含有一列数据，则按照一列的情形设置图例
