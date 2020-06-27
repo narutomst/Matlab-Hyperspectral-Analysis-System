8f933fb6ac2e583709692478767fc8eeda2e1add
narutomst <narutomst@163.com>
2020-06-27 22:41:13 +0800

    关闭所有的输出图像Figure，

--- saveAllFigure.m
+++ saveAllFigure.m
@@ -1,24 +1,31 @@
-function saveAllFigure(folderName,handles,fmt)
+function saveAllFigure(varargin)
+
 %保存所有图片
-if 1
+if nargin==3
     gc = gcf;
     ik = gc.Number;
     for i = ik:-1:2
         figure(i)
 %         filename = generateFilename('20200627', handles, ['_',num2str(i),'.fig']);
-        filename = generateFilename(folderName, handles, ['_',num2str(i),fmt]);
+        filename = generateFilename(varargin{1}, varargin{2}, ['_',num2str(i),varargin{3}]);
         saveas(gcf, filename);              
     end
 end
-if 0
-    %保存k张图片
-    in = k;
+
+%保存从当前图片gcf算起的k张图片
+if nargin==4
+    
+    in = varargin{4};
     gc = gcf;
     ik = gc.Number+1-in;
     for i = ik:(ik+in-1)
         figure(i)
         gc = gcf;
-        filename = generateFilename('20200627', handles, ['_',num2str(gc.Number),'.fig']);
-        saveas(gc, filename);              
+        filename = generateFilename(varargin{1}, varargin{2}, ['_',num2str(i),varargin{3}]);
+        try
+            saveas(gc, filename);    
+        catch
+        end
+        
     end
 end
