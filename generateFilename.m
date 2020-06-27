e0058f885ad709f36ff657bf2efcd4c27aa7c1f8
narutomst <narutomst@163.com>
2020-06-27 22:43:27 +0800

    小改动

--- generateFilename.m
+++ generateFilename.m
@@ -1,11 +1,12 @@
 %
 function filename = generateFilename(path, handles, fmt)
-        path = 'C:\Matlab练习\20200627';
+        %path = 'C:\Matlab练习\20200627';
+        hmenu4_1 = findobj(handles,'Label','加载数据');
         try            %拼接路径
-            path = fullfile(path, hmenu4_1.UserData.matName, hmenu4_1.UserData.drAlgorithm, hmenu4_1.UserData.cAlgorithm);
+            path = fullfile('C:\Matlab练习',path, hmenu4_1.UserData.matName, hmenu4_1.UserData.drAlgorithm, hmenu4_1.UserData.cAlgorithm);
         catch
         end
-            filename = [hmenu4_1.UserData.matName,'_',hmenu4_1.UserData.drAlgorithm,'_',hmenu4_1.UserData.cAlgorithm,'.xlsx'];
+            filename = [hmenu4_1.UserData.matName, '_', hmenu4_1.UserData.drAlgorithm, '_', hmenu4_1.UserData.cAlgorithm, fmt];
         try
             filename = fullfile(path,filename);%拼接路径
         catch
