e52e6320e5986d704774ee31c3f9008c48e622c9
narutomst <narutomst@163.com>
2020-07-17 23:02:50 +0800

    改变抽样规则为：训练样本按照分层抽样的原则，从每一类别中抽取rate比例的数量

--- createTwoTable.m
+++ createTwoTable.m
@@ -5,12 +5,29 @@ function [table1,table2,ind1,ind2] = createTwoTable(x2,lbs,rate)
 % 输入数据：x, 2维矩阵，每一行为一个样本，每一列为一个属性
 %                 y,列向量，为x每个样本所属的类的值
 %                 rate，标量（0～1），指定训练集所占的百分比
+dbstop if error
+C = numel(unique(lbs(:))); %总的类别数
+Cnum = zeros(C, 1);
+Cind = cell(1, C);
+for i = 1:C
+    Cnum(i) = sum(lbs==i);%统计每一类的样本数
+    Cind{i} = find(lbs==i); %统计每一类的行索引
+end
+% 估计每一类要抽取的样本数
+Cnum1 = round(Cnum*rate);
+% 从每一类索引中抽出若干个索引
+r = arrayfun(@(n, k) randperm(n, k), Cnum, Cnum1, 'UniformOutput', false);
+%r为C×1 cell，每个cell中保存的是每一类中被抽取的样本的行索引(行向量形式)
+%将r整理成为一个向量
+ind1 = [];
+for i = 1:C
+    a = Cind{i};
+   ind1 = [ind1; a(r{i})];
+end
+table1 = createTable(x2(ind1, :), lbs(ind1));%训练集
+ ind2 = [1: size(x2, 1)]';
+ ind2(ind1) = [];
 
-n = size(x2,1);        % 样本总数
-m = randperm(n);  % 生成1～n的整数随机排序向量
-k = round(rate*n);  % 获取训练集样本总数
-table1 = createTable(x2(m(1:k),:), lbs(m(1:k)));%训练集
-ind1 = m(1:k);
-table2 = createTable(x2(m(k+1:end),:), lbs(m(k+1:end)));%测试集
-ind2 = m(k+1:end);
+table2 = createTable(x2(ind2, :), lbs(ind2));%测试集
+
 % 数据t1和t2可以用于Classification App用于训练分类器和预测结果
