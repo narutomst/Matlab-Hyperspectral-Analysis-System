07b3eb5d823f43e156ea89ec81e5f98cfc876d78
narutomst <40396982+narutomst@users.noreply.github.com>
2022-05-09 21:52:28 +0800

    1. 对分类参数新增加了3个参数 2. 对importfile2()函数的功能做了更详细的注释

--- importfile2.m
+++ importfile2.m
@@ -1,5 +1,6 @@
 function paraTable_dr = importfile2(workbookFile, sheetName, dataLines)
-%IMPORTFILE1 导入电子表格中的数据
+%IMPORTFILE2 导入电子表格ParametersForDimReduceClassify.xlsx中的分类参数
+
 %  UNTITLED = IMPORTFILE1(FILE) 读取名为 FILE 的 Microsoft Excel
 %  电子表格文件的第一张工作表中的数据。  以表形式返回数据。
 %
@@ -9,8 +10,8 @@ function paraTable_dr = importfile2(workbookFile, sheetName, dataLines)
 %  DATALINES)按指定的行间隔读取指定工作表中的数据。对于不连续的行间隔，请将 DATALINES 指定为正整数标量或 N×2
 %  正整数标量数组。
 %
-%  示例:
-%  Untitled = importfile1("D:\MA毕业论文\ATrain_Record\20191002\降维参数统计.xlsx", "Sheet1", [2, 35]);
+%  示例: 导入分类参数
+%  Untitled = importfile1("C:\Matlab练习\Project20191002\ParametersForDimReduceClassify.xlsx", "Sheet2", [2, 7]);
 %
 %  另请参阅 READTABLE。
 %
@@ -20,7 +21,7 @@ function paraTable_dr = importfile2(workbookFile, sheetName, dataLines)
 
 % 如果未指定工作表，则将读取第一张工作表
 if nargin == 1 || isempty(sheetName)
-    sheetName = 1;
+    sheetName = 2;
 end
 
 % 如果未指定行的起点和终点，则会定义默认值。
@@ -30,25 +31,25 @@ end
 
 %% 设置导入选项并导入数据
 %opts1 = detectImportOptions(workbookFile,'sheet',sheetName);
-opts = spreadsheetImportOptions("NumVariables", 22);
+opts = spreadsheetImportOptions("NumVariables", 25);
 
 % 指定工作表和范围
 opts.Sheet = sheetName;
 %opts.RowNamesRange = "A" + dataLines(1, 1) + ":A" + dataLines(1, 2);
-opts.RowNamesRange = "A2";
-opts.DataRange = "B" + dataLines(1, 1) + ":W" + dataLines(1, 2);
+opts.RowNamesRange = "A" + dataLines(1, 1);
+opts.DataRange = "B" + dataLines(1, 1) + ":Z" + dataLines(1, 2);
 
 
 % 指定列名称和类型
 opts.VariableNames = ["dimReduce", "rate","app" ,"executionTimes" "trainFcn","hiddenNum","transferFcn","showWindow",...
-"plotperform", "plottrainstate", "ploterrhist", "plotconfusion", "plotroc", "hiddenLayerNum"...
-	"hiddenNum1","transferFcn1","hiddenNum2","transferFcn2","hiddenNum3", ...
-    "transferFcn3","hiddenNum4","transferFcn4"];
+"plotperform", "plottrainstate", "ploterrhist", "plotconfusion", "plotroc", "hiddenLayerNum","hiddenNum1","transferFcn1",...
+	"hiddenNum2","transferFcn2","hiddenNum3", "transferFcn3","hiddenNum4","transferFcn4",...
+    "hiddenNumOptimization","startNum","stopNum"];
 
 opts.VariableTypes = ["logical",   "double","double","double",           "string",  "double",         "string",        "logical",...
-"logical",        "logical",            "logical",       "logical",            "logical",  "double", ...
-	"double",         "string",           "double",         "string",          "double", ...
-    "string",          "double",           "string"];
+"logical",        "logical",            "logical",       "logical",            "logical",  "double",                "double",         "string",...
+    "double",         "string",          "double",           "string",           "double",           "string", ...
+    "logical",                             "double",  "double"];
 
 % 指定文件级属性
 opts.ImportErrorRule = 'omitvar';
